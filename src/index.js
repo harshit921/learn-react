0ea37a93c70f204f06bf3eccbce8ed3fe5b19328
Harshit Agarwal <harshit_agrawal36@yahoo.com>
2017-10-05 06:41:59 -0700

    Routing React Components

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@ import {render} from 'react-dom';
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import {BrowserRouter , Match , Miss} from 'react-router';
+import NotFound from './components/NotFound';
 
-render(<App/> ,document.querySelector('#main'));
+const Root = () => {
+	return (
+		<BrowserRouter>
+		<div>
+			<Match exactly pattern="/" component={StorePicker} />
+		<Match pattern= "/store/:storeId" component={App} />
+		<Miss component={NotFound} />
+		</div>
+		</BrowserRouter>
+		)
+}
+
+
+render(<Root/> ,document.querySelector('#main'));
