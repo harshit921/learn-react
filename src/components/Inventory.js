9240fbd0636a8b12dd3295d1b92fc7e27f7ef0de
Harshit Agarwal <harshit_agrawal36@yahoo.com>
2017-10-12 00:38:09 -0700

    Adding sync to Inventory and App

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,10 +2,42 @@ import React from 'react';
 import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
+
+	constructor() {
+		super();
+		this.renderInventory = this.renderInventory.bind(this);
+	}
+
+  handleChange(e, key) {
+    const fish = this.props.fishes[key];
+    const updatedFish = {
+      ...fish,
+      [e.target.name]: e.target.value
+    }
+    this.props.updateFish(key, updatedFish);
+  }
+
+  renderInventory(key){
+  	const fish = this.props.fishes[key];
+  	return(
+  		<div className="fish-edit" key={key}>
+  			<input type="text" name="name" value={fish.name} placeholder="Fish Name" onChange={(e) => this.handleChange(e, key)} />
+  			<input type="text" name="price" value={fish.price} placeholder="Fish Name" onChange={(e) => this.handleChange(e, key)} />
+  			<select name="status" type="text"  value={fish.status}placeholder="fish Status" onChange={(e) => this.handleChange(e, key)}>
+  				<option value="available">Fresh!</option>
+  				<option value="unavailable">Sold Out</option>
+  			</select>
+			<textarea name="desc" type="text" value={fish.desc} placeholder="Fish Description" onChange={(e) => this.handleChange(e, key)} ></textarea>
+  			<input type="text" name="image" value={fish.image} placeholder="Fish Image" onChange={(e) => this.handleChange(e, key)} />
+		 </div>
+  		)
+  }
+
   render() {
     return (
       <div>
         <h2>Inventory</h2>
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
         <AddFishForm addFish={this.props.addFish}/>
         <button onClick={this.props.loadSamples} > Load Sample Fishes </button>
       </div>
