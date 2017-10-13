a474cad580e10f6b776b5ace765a3aaf340b49e1
Harshit Agarwal <harshit_agrawal36@yahoo.com>
2017-10-13 11:39:29 -0700

    adding animations

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { formatPrice } from '../helpers';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Order extends React.Component {
   constructor() {
@@ -17,7 +18,19 @@ class Order extends React.Component {
 
     return (
       <li key={key}>
-        <span>{count}lbs {fish.name}  {removeButton}</span>
+        <span>
+        	<CSSTransitionGroup
+        		component="span"
+        		className="count"
+        		transitionName="count"
+        		transitionEnterTimeout={250}
+        		transitionLeaveTimeout={250}
+        	>
+				<span key={count}> {count}</span>
+        	</CSSTransitionGroup>
+        
+        lbs {fish.name}  {removeButton}
+        </span>
         <span className="price">{formatPrice(count * fish.price)}</span>
 
       </li>
@@ -38,13 +51,13 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
-        <ul className="order">
+        <CSSTransitionGroup component="ul" transitionName="order" transitionEnterTimeout={5000} transitionLeaveTimeout={5000} className="order">
           {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {formatPrice(total)}
           </li>
-        </ul>
+        </CSSTransitionGroup>
 
       </div>
     )
